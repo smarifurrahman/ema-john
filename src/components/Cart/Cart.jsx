e9eca9d6c214081f6fd3061ca9e6e6a1b8676f52
S M Arifur Rahman <smarifurrahman82@gmail.com>
2023-03-29 17:41:48 +0600

    added product summary componenet and calculation done

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Cart.css';
+
+const Cart = ({ cart }) => {
+    // const totalPrice = cart.reduce((previous, current) => previous + current.price, 0);
+    // const totalShipping = cart.reduce((previous, current) => previous + current.shipping, 0);
+
+    let totalPrice = 0;
+    let totalShipping = 0;
+    for (const product of cart) {
+        totalPrice += product.price;
+        totalShipping += product.shipping;
+    }
+
+    return (
+        <div className='cart'>
+            <h4>Order Summary</h4>
+            <p>Selected Items: {cart.length}</p>
+            <p>Total Price: {totalPrice}</p>
+            <p>Total Shipping: {totalShipping}</p>
+            <h5>Grand Total: ${totalPrice + totalShipping}</h5>
+        </div>
+    );
+};
+
+export default Cart;
