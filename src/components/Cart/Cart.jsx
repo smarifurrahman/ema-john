b4cd0cbbe7fd87427123e8a72aa6be3baa0ab40f
S M Arifur Rahman <smarifurrahman82@gmail.com>
2023-03-30 01:25:20 +0600

    calculation from localStorage and add to cart clicked fixed

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,23 +2,22 @@ import React from 'react';
 import './Cart.css';
 
 const Cart = ({ cart }) => {
-    // const totalPrice = cart.reduce((previous, current) => previous + current.price, 0);
-    // const totalShipping = cart.reduce((previous, current) => previous + current.shipping, 0);
-
+    let quantity = 0;
     let totalPrice = 0;
     let totalShipping = 0;
     for (const product of cart) {
-        totalPrice += product.price;
+        quantity += product.quantity;
+        totalPrice += product.price * product.quantity;
         totalShipping += product.shipping;
     }
-    // const tax = +(totalPrice * 7 / 100).toFixed(2);
+
     const tax = totalPrice * 7 / 100;
     const grandTotal = totalPrice + totalShipping + tax;
 
     return (
         <div className='cart'>
             <h4>Order Summary</h4>
-            <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {quantity}</p>
             <p>Total Price: ${totalPrice}</p>
             <p>Total Shipping: ${totalShipping}</p>
             <p>Tax: ${tax.toFixed(2)}</p>
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -14,14 +14,40 @@ const Shop = () => {
             .then(data => setProducts(data))
     }, [])
 
+    // calculate when site loaded.
     useEffect(() => {
         const storedCart = getShoppingCart();
-    }, [])
+        const savedCart = [];
 
+        for (const id in storedCart) {
+            const addedProduct = products.find(product => product.id === id);
+            if (addedProduct) {
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                savedCart.push(addedProduct);
+            }
+        }
+
+        setCart(savedCart);
+    }, [products])
+
+    // calculate when add to Cart clicked.
     const handleAddToCart = (product) => {
-        const newCart = [...cart, product];
+        let newCart = [];
+        const exist = cart.find(pd => pd.id === product.id);
+
+        if (!exist) {
+            product.quantity = 1;
+            newCart = [...cart, product];
+        }
+        else {
+            exist.quantity += 1;
+            const remaining = cart.filter(pd => pd.id !== product.id);
+            newCart = [...remaining, exist];
+        }
+
         setCart(newCart);
-        addToDb(product.id)
+        addToDb(product.id);
     }
 
     return (
