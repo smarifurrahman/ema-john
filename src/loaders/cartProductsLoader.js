0e7e4096a2baf33e0aec7aa289a8c44a66f9bd25
S M Arifur Rahman <smarifurrahman82@gmail.com>
2023-04-10 00:56:55 +0600

    cart data successfully loaded in orders page

--- src/loaders/cartProductsLoader.js
+++ src/loaders/cartProductsLoader.js
@@ -1,8 +1,28 @@
+import { getShoppingCart } from "../utilities/fakedb";
+
 const cartProductsLoader = async () => {
     const loadedProducts = await fetch('products.json');
     const products = await loadedProducts.json();
-    console.log(products);
-    return products;
+
+    // if cart data is in database, we have to use async await
+    const storedCart = getShoppingCart();
+    const savedCart = [];
+
+    for (const id in storedCart) {
+        const addedProduct = products.find(product => product.id === id);
+        if (addedProduct) {
+            const quantity = storedCart[id];
+            addedProduct.quantity = quantity;
+            savedCart.push(addedProduct);
+        }
+    }
+
+    return savedCart;
+
+    // if we need to send two things
+    // return [products, savedCart];
+    // another option
+    // return { products, cart: savedCart };
 }
 
 export default cartProductsLoader;
