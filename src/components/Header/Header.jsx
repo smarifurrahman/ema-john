562dc09f8e1e6ba97d6e47a73f1ce77bbff52f74
S M Arifur Rahman <smarifurrahman82@gmail.com>
2023-05-01 17:29:18 +0600

    signup, login and signout implemented

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Header.css';
 import logo from '../../images/Logo.svg';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../providers/AuthProvider';
 
 const Header = () => {
+    const { user, logOut } = useContext(AuthContext);
+
+    const handleLogout = () => {
+        logOut()
+            .then(result => { console.log('Logout Success') })
+            .catch(error => console.error(error));
+    }
+
     return (
         <nav className='header'>
             <div>
@@ -15,6 +24,7 @@ const Header = () => {
                 <Link to="/inventory">Inventory</Link>
                 <Link to="/login">Login</Link>
                 <Link to="/signup">Sign Up</Link>
+                {user && <span className='text-white'>Welcome {user.email} <button onClick={handleLogout} className='btn'>Sign Out</button></span>}
             </div>
         </nav>
     );
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import './SignUp.css';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../providers/AuthProvider';
 
 const SignUp = () => {
+    const [error, setError] = useState('');
+    const { createUser } = useContext(AuthContext);
+
+    const handleSignUp = (event) => {
+        event.preventDefault();
+
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        const confirm = form.confirm.value;
+        console.log(email, password, confirm);
+
+        setError('');
+
+        if (password !== confirm) {
+            setError('Your password did not matched');
+            return;
+        }
+        else if (password.length < 6) {
+            setError('Password must be 6 characters or longer');
+            return;
+        }
+
+        createUser(email, password)
+            .then(result => {
+                const loggedUser = result.user;
+                console.log(loggedUser)
+            })
+            .catch(error => {
+                console.log(error);
+                setError(error.message);
+            })
+    }
+
     return (
         <div className='form-container'>
             <h2 className='form-title'>Sign Up</h2>
-            <form>
+            <form onSubmit={handleSignUp}>
                 <div className="form-control">
                     <label htmlFor="email">Email</label>
                     <input type="email" name="email" id="" required />
@@ -16,12 +51,14 @@ const SignUp = () => {
                     <input type="password" name="password" id="" required />
                 </div>
                 <div className="form-control">
-                    <label htmlFor="password">Confirm Password</label>
+                    <label htmlFor="confirm">Confirm Password</label>
                     <input type="password" name="confirm" id="" required />
                 </div>
                 <input className='btn-submit' type="submit" value="Sign Up" />
             </form>
             <p><small>Already have an account? <Link to="/login"><span>Login</span></Link></small></p>
+
+            <p className='text-error'>{error}</p>
         </div>
     );
 };
