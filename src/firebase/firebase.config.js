2786f2b2aca463a7f85171bfbb4ba9f3a3355d3c
S M Arifur Rahman <smarifurrahman82@gmail.com>
2023-05-01 15:10:58 +0600

    firebase added and signup route added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyADrroXZbNPuODZwgciwbzix38aK6dcw9I",
+  authDomain: "ema-john-b8dec.firebaseapp.com",
+  projectId: "ema-john-b8dec",
+  storageBucket: "ema-john-b8dec.appspot.com",
+  messagingSenderId: "1016087491255",
+  appId: "1:1016087491255:web:00b5472fd4b8c8a8e79332"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
